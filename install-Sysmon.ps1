d671f1183b44024bf585f8ef2bf0d264fcaa4bf2
XuanTram <50724075+XuanTram@users.noreply.github.com>
2022-04-05 15:13:38 +0700

    Xoá file

--- install-Sysmon.ps1
+++
@@ -1,9 +0,0 @@
-﻿<#
-Script: Install sysmon to monitor on server 
-Author: DMH
-Date: 31/03/2022
-#>
-
-Set-Location -Path 'C:\Users\huyendm\Desktop\Sysmon' #Di chuyen den folder chua sysmon.exe
-.\sysmon64.exe -accepteula -i sysmonconfig-export.xml #Chay ct 
-Write-Host "Sysmon Installed!"  #Ket thuc chuong trinh
